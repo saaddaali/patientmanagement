56cbe7d22c2e00ad5303e63512db007b6dd18632
SoukainaZ <soukainazaid01@gmail.com>
2024-12-25 20:19:24 +0100

    'fixConflit'

--- backend-ms1/src/main/java/ma/zyn/app/ws/facade/doctor/sensor/CapteurTypeRestDoctor.java
+++ backend-ms1/src/main/java/ma/zyn/app/ws/facade/doctor/sensor/CapteurTypeRestDoctor.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/doctorgit/capteurType/")
+@RequestMapping("/api/doctor/capteurType/")
 public class CapteurTypeRestDoctor {
 
 
