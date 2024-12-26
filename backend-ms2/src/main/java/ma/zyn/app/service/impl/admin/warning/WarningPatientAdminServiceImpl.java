67b32d17782d00072cf0334c42692334d23b6dde
saaddaali <medsaad6@gmail.com>
2024-12-26 18:36:33 +0100

    Fix SOS Button

--- backend-ms2/src/main/java/ma/zyn/app/service/impl/admin/warning/WarningPatientAdminServiceImpl.java
+++ backend-ms2/src/main/java/ma/zyn/app/service/impl/admin/warning/WarningPatientAdminServiceImpl.java
@@ -145,7 +145,7 @@ public class WarningPatientAdminServiceImpl implements WarningPatientAdminServic
 
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class, readOnly = false)
     public WarningPatient create(WarningPatient t) {
-        if (t.getPatient() != null) {
+        if (t.getPatient() == null) {
             t.getPatient().setId(null);
             t.setPatient(patientService.create(t.getPatient()));
         }
